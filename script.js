5465abd17451aec162886d69f859204fd0659f74
rafaelapizzolotto <164499957+rafaelapizzolotto@users.noreply.github.com>
2025-03-21 09:29:47 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -16,3 +17,7 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+  botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
